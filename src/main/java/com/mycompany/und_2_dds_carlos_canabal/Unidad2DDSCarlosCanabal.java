8883f4f8e4a27bef50d3574159995766c793e405
canabaldev1 <canabal.dev1@gmail.com>
2025-09-29 01:29:43 -0500

    Inicializado proyecto

---
+++ src/main/java/com/mycompany/und_2_dds_carlos_canabal/Unidad2DDSCarlosCanabal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.und_2_dds_carlos_canabal;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Unidad2DDSCarlosCanabal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
