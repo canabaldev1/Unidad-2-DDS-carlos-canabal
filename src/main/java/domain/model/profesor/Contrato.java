00e388cb0c1a144ed91150e12c57a850c688ac1a
canabaldev1 <canabal.dev1@gmail.com>
2025-09-29 22:33:36 -0500

    creacion de agregado profesor con entidades y VOs

---
+++ src/main/java/domain/model/profesor/Contrato.java
@@ -0,0 +1,13 @@
+package domain.model.profesor;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Contrato {
+
+    // Atributos de la entidad
+    // Constructores, tanto para entidad nueva como para recrear con el mapper
+    // Getters y Setters
+    // Metodos del atributo
+}
