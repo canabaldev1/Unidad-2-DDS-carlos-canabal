aad1c502cbe8bd26fd84acec6ffdf3dd40bba815
canabaldev1 <canabal.dev1@gmail.com>
2025-09-29 23:08:45 -0500

    creacion de puertos de salida - repositorios

---
+++ src/main/java/domain/port/out/repositories/RepositorioDeCursos.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package domain.port.out.repositories;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public interface RepositorioDeCursos {
+    
+    // Interfaz que utiliza el adaptador del repositorio
+    
+}
---
+++ src/main/java/domain/port/out/repositories/RepositorioDeProfesores.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package domain.port.out.repositories;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public interface RepositorioDeProfesores {
+    
+        // Interfaz que utiliza el adaptador del repositorio
+}
