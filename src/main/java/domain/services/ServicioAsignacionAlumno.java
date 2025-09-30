0f37605ae57176fecd1f6722f2964ea15e751a45
canabaldev1 <canabal.dev1@gmail.com>
2025-09-29 22:48:36 -0500

    creacion de servicios de dominio

---
+++ src/main/java/domain/services/ServicioAsignacionAlumno.java
@@ -0,0 +1,18 @@
+package domain.services;
+
+import domain.model.alumno.Alumno;
+import domain.model.curso.Curso;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class ServicioAsignacionAlumno {
+    
+    public void asignarAlumno(Curso curso, Alumno alumno) {
+
+        // Relaciona al profesor que recibe por parametro al respectivo curso
+        
+    }
+
+}
---
+++ src/main/java/domain/services/ServicioAsignacionProfesor.java
@@ -0,0 +1,17 @@
+package domain.services;
+
+import domain.model.curso.Curso;
+import domain.model.profesor.Profesor;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class ServicioAsignacionProfesor {
+
+    public void asignarProfesor(Curso curso, Profesor profesor) {
+
+        // Relaciona al profesor que recibe por parametro al respectivo curso
+        
+    }
+}
