809e14782eb81a394429f853dcc994903a35ae1b
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2025-11-10 10:59:55 -0500

    actualizar pom.xml para utilizar el núcleo de Spring Boot, ajustar dependencias y mejorar la configuración de escaneo de entidades

--- src/main/java/com/mycompany/und_2_dds_carlos_canabal/Unidad2DDSCarlosCanabalApplication.java
+++ src/main/java/com/mycompany/und_2_dds_carlos_canabal/Unidad2DDSCarlosCanabalApplication.java
@@ -2,8 +2,18 @@ package com.mycompany.und_2_dds_carlos_canabal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "application",
+        "domain",
+        "infraestructure.adapter.in",
+        "infraestructure.adapter.out",
+        "com.mycompany.und_2_dds_carlos_canabal"
+})
+@EntityScan(basePackages = {"infraestructure.adapter.out.repositories"})
+@EnableJpaRepositories(basePackages = {"infraestructure.adapter.out.repositories"})
 public class Unidad2DDSCarlosCanabalApplication {
 
     public static void main(String[] args) {
