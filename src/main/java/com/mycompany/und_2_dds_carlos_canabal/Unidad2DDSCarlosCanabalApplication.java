36fd5e14fc81bdb6c733d7c198803389ac6052b1
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2025-11-19 08:58:44 -0500

    refactor: limpiando imports y comentarios en deshuso

--- src/main/java/com/mycompany/und_2_dds_carlos_canabal/Unidad2DDSCarlosCanabalApplication.java
+++ src/main/java/com/mycompany/und_2_dds_carlos_canabal/Unidad2DDSCarlosCanabalApplication.java
@@ -1,17 +1,10 @@
 package com.mycompany.und_2_dds_carlos_canabal;
 
-import domain.model.curso.Curso;
-import domain.valueobjects.curso.Aula;
-import domain.valueobjects.profesor.ProfesorId;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.context.ConfigurableApplicationContext;
-import application.port.in.RegistrarCursoUseCase;
 import infraestructure.adapter.in.swing.JFrameVentanaPrincipal;
-
-import java.time.LocalDate;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(scanBasePackages = {
@@ -34,32 +27,9 @@ public class Unidad2DDSCarlosCanabalApplication {
                 .run(args);
 
         System.out.println("Aplicación Spring Boot iniciada correctamente\n.");
-        
+
         JFrameVentanaPrincipal ventanaPrincipal = context.getBean(JFrameVentanaPrincipal.class);
-        
+
         ventanaPrincipal.setVisible(true);
-
-        //prueba de post para crear un curso nuevo
-/*      
-        System.out.println("Implementación de la interfaz RegistrarCursoUseCase: ");
-
-        RegistrarCursoUseCase registrarCurso = context.getBean(RegistrarCursoUseCase.class);
-
-        try {
-            Curso curso = registrarCurso.registrarCurso(
-                    "esternocleidomastoideo",
-                    30,
-                    LocalDate.of(2024, 9, 1),
-                    LocalDate.of(2025, 6, 30),
-                    new Aula("Aula 101", "planta 2b", "Edificio Central",
-                            "laboratorio de redes", 23),
-                    new ProfesorId("prof-12345")
-            );
-            System.out.println("Curso registrado exitosamente: ");
-        } catch (Exception e) {
-            System.out.println("Error al registrar el curso: " + e.getMessage());
-        }
-
-        context.close();*/
     }
 }
