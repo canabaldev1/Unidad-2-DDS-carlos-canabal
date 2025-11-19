051c372bd89195573bc1ac17579279f71b31654c
canabaldev1 <canabal.dev1@gmail.com>
2025-11-18 23:12:03 -0500

    creado comando, use case y servicio de aplicacion

---
+++ src/main/java/application/services/CrearProfesorService.java
@@ -0,0 +1,51 @@
+package application.services;
+
+import application.port.in.CrearProfesorUseCase;
+import application.port.in.command.CrearProfesorCommand;
+import domain.model.profesor.Profesor;
+import domain.port.out.repositories.RepositorioDeProfesores;
+import domain.valueobjects.profesor.ProfesorId;
+import domain.valueobjects.shared.DatosContacto;
+import domain.valueobjects.shared.DatosPersonales;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author lm-carlos
+ */
+@Service
+@Transactional
+public class CrearProfesorService implements CrearProfesorUseCase{
+private final RepositorioDeProfesores repositorioDeProfesores;
+
+    public CrearProfesorService(RepositorioDeProfesores repositorioDeProfesores) {
+        this.repositorioDeProfesores = repositorioDeProfesores;
+    }
+
+    @Override
+    public void handle(CrearProfesorCommand command) {
+        // 1. CREAR VALUE OBJECTS
+        DatosPersonales datosPersonales = new DatosPersonales(
+            command.nombre(),
+            command.apellido(),
+            command.fechaNacimiento()
+        );
+        
+        DatosContacto datosContacto = DatosContacto.crear(
+            command.telefono(),
+            command.email(),
+            command.direccion()
+        );
+
+        // CREAR ID ÚNICO
+        ProfesorId profesorId = ProfesorId.generate();
+
+        // CREAR ENTIDAD DE DOMINIO
+        Profesor profesor = new Profesor(profesorId, datosPersonales, command.especialidad());
+
+
+        // 5. GUARDAR
+        repositorioDeProfesores.guardar(profesor);
+    }
+}
--- src/main/java/domain/port/out/repositories/RepositorioDeProfesores.java
+++ src/main/java/domain/port/out/repositories/RepositorioDeProfesores.java
@@ -4,11 +4,18 @@
  */
 package domain.port.out.repositories;
 
+import domain.model.profesor.Profesor;
+import java.util.List;
+import java.util.Optional;
+
 /**
  *
  * @author lm-carlos
  */
 public interface RepositorioDeProfesores {
-    
-        // Interfaz que utiliza el adaptador del repositorio
+
+    // Interfaz que utiliza el adaptador del repositorio
+    Profesor guardar(Profesor profesor);
+    Optional<Profesor> buscarPorId(String id);
+    List<Profesor> listar();
 }
