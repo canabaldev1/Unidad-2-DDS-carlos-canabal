660efb2e814bb68e2e70daf875fecd4a7ff36696
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2025-11-21 15:06:06 -0500

    agregando visualizacion de ventas alumno y curso

--- src/main/java/infraestructure/adapter/in/swing/JFrameVentanaPrincipal.java
+++ src/main/java/infraestructure/adapter/in/swing/JFrameVentanaPrincipal.java
@@ -4,6 +4,7 @@
  */
 package infraestructure.adapter.in.swing;
 
+import infraestructure.adapter.in.swing.Alumnos.JFrameAlumnos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -38,8 +39,8 @@ public class JFrameVentanaPrincipal extends javax.swing.JFrame {
 
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        CursosButton = new javax.swing.JButton();
-        AlumnosButton = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -58,18 +59,25 @@ public class JFrameVentanaPrincipal extends javax.swing.JFrame {
             }
         });
 
-        CursosButton.setText("Cursos");
-        CursosButton.addActionListener(new java.awt.event.ActionListener() {
+        jButton3.setText("Cursos");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 CursosButtonActionPerformed(evt);
             }
         });
 
-        AlumnosButton.setText("Alumnos");
+        jButton4.setText("Alumnos");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JFrameAlumnos ventanaAlumnos = context.getBean(JFrameAlumnos.class);
+                ventanaAlumnos.setVisible(true);
+            }
+        });
 
         jLabel1.setFont(new java.awt.Font("Liberation Sans", 1, 18)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("SOFTWARE DE GESTION ESCOLAR");
+
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -78,11 +86,12 @@ public class JFrameVentanaPrincipal extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(55, 55, 55)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(AlumnosButton, javax.swing.GroupLayout.DEFAULT_SIZE, 196, Short.MAX_VALUE)
-                    .addComponent(CursosButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, 196, Short.MAX_VALUE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(89, 89, 89))
             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 539, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
@@ -92,13 +101,15 @@ public class JFrameVentanaPrincipal extends javax.swing.JFrame {
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(58, 58, 58)
                 .addComponent(jButton1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton2)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton3)
                 .addGap(18, 18, 18)
-                .addComponent(jButton2)
-                .addGap(18, 18, 18)
-                .addComponent(CursosButton)
-                .addGap(18, 18, 18)
-                .addComponent(AlumnosButton)
-                .addContainerGap(140, Short.MAX_VALUE))
+                .addComponent(jButton4)
+                .addContainerGap(136, Short.MAX_VALUE))
         );
 
         pack();
@@ -111,8 +122,16 @@ public class JFrameVentanaPrincipal extends javax.swing.JFrame {
     private void CursosButtonActionPerformed(java.awt.event.ActionEvent evt) {
         JFrameCursos ventanaCursos = context.getBean(JFrameCursos.class);
         ventanaCursos.setVisible(true);
-        this.setVisible(false);
         }
+
+    @Autowired
+    private JFrameCrearProfesor ventanaCrearProfesor;
+
+    private void AbrirCrearUsuario(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AbrirCrearUsuario
+        // TODO add your handling code here:
+        ventanaCrearProfesor.setLocationRelativeTo(this);
+        ventanaCrearProfesor.setVisible(true);
+    }//GEN-LAST:event_AbrirCrearUsuario
 
     /**
      * @param args the command line arguments
@@ -135,8 +154,8 @@ public class JFrameVentanaPrincipal extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton CursosButton;
-    private javax.swing.JButton AlumnosButton;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
